c6c4a555f36b37ce65e08a3e6409027dac323099
Tom Ridge <tom.j.ridge@googlemail.com>
2018-12-05 17:13:58 +0000

    +

--- src/global.ml
+++ src/global.ml
@@ -1,6 +1,10 @@
 (** Keep track of state eg config, bool vars etc etc *)
 
+let debug_global = ref false
+
 let register ~(name:string) a = 
-  Printf.printf "%s: registered value with name %s\n%!" __MODULE__ name;
+  (if !debug_global then 
+     Printf.printf "%s: registered value with name %s\n%!" __MODULE__ name
+   else ());
   a
 
