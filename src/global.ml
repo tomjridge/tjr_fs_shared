03f0b1993c59ad92a79bb009715192032c63d2e7
Tom Ridge <tom.j.ridge@googlemail.com>
2018-12-05 17:18:30 +0000

    +

--- src/global.ml
+++ src/global.ml
@@ -1,5 +1,7 @@
 (** Keep track of state eg config, bool vars etc etc *)
 
+(** This flag, if true, forces names of global vars to be printed when
+   registered *)
 let debug_global = ref false
 
 let register ~(name:string) a = 
