f29b0b73269b01119ba20c36a5e576aaa675d7aa
Tom Ridge <tom.j.ridge@googlemail.com>
2018-12-05 17:38:08 +0000

    +

--- src/global.ml
+++ src/global.ml
@@ -4,7 +4,18 @@
    registered *)
 let debug_global = ref false
 
+module Internal = struct
+
+  let names = ref []
+
+  let print_all_names () = 
+    List.iter (fun name -> print_endline name) (!names)
+
+end
+open Internal
+
 let register ~(name:string) a = 
+  names:=name::!names;
   (if !debug_global then 
      Printf.printf "%s: registered value with name %s\n%!" __MODULE__ name
    else ());
