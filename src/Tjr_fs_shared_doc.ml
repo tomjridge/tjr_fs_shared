ecedde2e4f574890b2c258d270b7e269d1b1f930
Tom Ridge <tom.j.ridge@googlemail.com>
2019-01-21 13:26:46 +0000

    +

--- src/Tjr_fs_shared_doc.ml
+++ src/Tjr_fs_shared_doc.ml
@@ -1,5 +1,7 @@
 (** A summary of the main types provided by this package. *)
 
+(** {0 Types } *)
+
 include Blk_dev_ops_type
 
 include Blk_sz_type
@@ -15,3 +17,14 @@ include Map_ops_type
 
 type ss = Small_string.ss
 
+
+
+(** {0 Implementations} *)
+
+(** Note the following implementations:
+
+- {!Blk_dev_in_mem}, {!Blk_dev_on_fd}, {!Block_ops.make_string_block_ops}
+- {!Kv_op.default_kvop_map_ops}
+
+
+*)
