6102385adb105600321c3eb0577b00a876b5ec6c
Tom Ridge <tom.j.ridge@googlemail.com>
2020-01-22 16:08:11 +0000

    +

--- src/shared_intf.ml
+++ src/shared_intf.ml
@@ -4,3 +4,21 @@ type ('a,'b) initial_state_and_ops = {initial_state:'a; ops:'b}
 
 type ba_buf = 
   (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+
+
+
+(** {2 Marshalling and unmarshalling.}  *)
+
+module Off_ = struct
+  type off = int
+end
+open Off_
+
+type ('a,'buf) mshl = {
+  marshal   : 'a -> 'buf*off -> 'buf*off;
+  unmarshal : 'buf -> off -> 'a * off;
+  max_sz    : int; 
+}
+(** max_sz: maximum size of a marshalled elt *)
+
+
