58b9c8de7cfca2d351fc831cfbc0f7b90dd6e6b5
Tom Ridge <tom.j.ridge@googlemail.com>
2020-02-06 18:29:15 +0000

    +

--- src/d_blocks_etc/blk_dev_factory.ml
+++ src/d_blocks_etc/blk_dev_factory.ml
@@ -1,4 +1,12 @@
 open Blk_intf
+open Shared_intf
+
+(** Internal: blk_id is Blk_id_as_int *)
+
+module Internal = struct
+  type blk_id = Blk_id_as_int.blk_id
+end
+open Internal
 
 (** The factory takes an arg of the form AN... and returns a result of the form RN...; add further args and results here *)
 
@@ -14,17 +22,22 @@ type arg =
 
   | A4_ba_4096_lwt_mem
     (** blk is a bigarray_4096; monad is lwt; blk_dev in mem *)
+    
+  | A5_blk_ba__lwt_fd
+    (** blk is a ba_4096; monad is lwt; blk_dev on fd *)
+
 
 type 'blk r_3_4 = 
-((Blk_id_as_int.blk_id,'blk) Tjr_map.With_pervasives_compare.map_with_pervasives_compare, lwt)
+((blk_id,'blk) Tjr_map.With_pervasives_compare.map_with_pervasives_compare, lwt)
 with_state
--> (Blk_id_as_int.blk_id, 'blk, lwt) blk_dev_ops
+-> (blk_id, 'blk, lwt) blk_dev_ops
 
 type res = 
-  | R1 of (Lwt_unix.file_descr -> (Blk_id_as_int.blk_id,string,lwt)blk_dev_ops)
-  | R2 of (Lwt_unix.file_descr -> (Blk_id_as_int.blk_id,bytes,lwt)blk_dev_ops)
+  | R1 of (Lwt_unix.file_descr -> (blk_id,string,lwt)blk_dev_ops)
+  | R2 of (Lwt_unix.file_descr -> (blk_id,bytes,lwt)blk_dev_ops)
   | R3 of bytes r_3_4
-  | R4 of Buf.Buf_as_bigarray.ba_buf r_3_4
+  | R4 of ba_buf r_3_4
+  | R5 of (Lwt_unix.file_descr -> (blk_id,ba_buf,lwt)blk_dev_ops)
 
 let make = function
   | A1_string_4096_lwt_fd -> 
@@ -43,7 +56,7 @@ let make = function
     in
     R3 f
   | A4_ba_4096_lwt_mem ->
-    let blk_ops = Common_blk_ops.Bytes_.make ~blk_sz:blk_sz_4096 in
+    (* let blk_ops = Common_blk_ops.Bytes_.make ~blk_sz:blk_sz_4096 in *)
     let f with_state = (
       Blk_dev_in_mem.make_blk_dev_in_mem
         ~monad_ops:lwt_monad_ops
@@ -51,4 +64,7 @@ let make = function
         ~with_state)
     in
     R4 f
-
+  | A5_blk_ba__lwt_fd ->
+    let blk_ops = Blk_factory.(make A3_ba_4096 |> fun (R3 x) -> x) in
+    R5 (fun fd -> Blk_dev_on_fd.make_with_lwt ~blk_ops ~fd)
+[@@warning "-8"]
