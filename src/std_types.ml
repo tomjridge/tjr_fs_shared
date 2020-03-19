396c0138d08a5fbf61320eecebb88997b99bd21c
Tom Ridge <tom.j.ridge@googlemail.com>
2020-03-19 19:20:09 +0000

    +

--- src/std_types.ml
+++ src/std_types.ml
@@ -9,6 +9,7 @@ type blk_id = Blk_intf.Blk_id_as_int.blk_id[@@deriving bin_io, yojson]
 type r = blk_id[@@deriving bin_io, yojson]
 let r_cmp : r -> r -> int = Stdlib.compare
 type buf = ba_buf
+let buf_ops = Buf_factory.Buf_as_bigarray.ba_buf_ops
 
 let monad_ops = lwt_monad_ops
 
@@ -22,5 +23,6 @@ module B = Blk_intf.Blk_id_as_int
 let r_size = 9 (* max size of r=blk_id when marshalled *)
 
 open Blk_intf
+(* FIXME perhaps just blk_dev_ops and blk_allocator_ops ? *)
 type nonrec std_blk_dev_ops = (blk_id,blk,t)blk_dev_ops
 type nonrec std_blk_allocator_ops = (r,t)blk_allocator_ops
