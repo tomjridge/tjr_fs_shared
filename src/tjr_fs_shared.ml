6ac276ba12109bf9282c2c24aca4600217bfc5df
Tom Ridge <tom.j.ridge@googlemail.com>
2020-05-18 18:31:08 +0100

    +

--- src/tjr_fs_shared.ml
+++ src/tjr_fs_shared.ml
@@ -59,6 +59,78 @@ module Blk_dev_factory = Blk_dev_factory
 
 module Root_block = Root_block
 
+
+
+(** {2 Operations extended with a "sync" operation} *)
+
+(** This is a common requirement on operations from the "lower" layer:
+   there is a need to push changes that have been submitted to the
+   lower layer, so that they are actually persistent. Maybe "flush"
+   would be a better name. *)
+type ('a,'t) with_sync = <
+  get: 'a;
+  sync: unit -> (unit,'t)m
+>
+
+
+(** {2 Note on sync and the blk device layer} *)
+
+(**
+
+A traditional block device accepts (read and) write requests. Write
+   requests can be reordered, although writes for a particular blk id
+   will commit in order.
+
+A barrier operation enforces that it is not the case that a request
+   before the barrier is reordered with a request after the barrier.
+
+A sync operation requests that all writes are actually made
+   persistent.
+
+Of course, many devices do not implement these things properly.
+
+Newer interfaces, such as (variants of) TCQ and NVMe, are more
+   complicated. Typically there are queues of operations. Barrier and
+   flush commands can be per-queue. It may be possible to request
+   notification for when some operation actually completes (say, when
+   a write is made persistent). There is often a "Forced Unit Access
+   (FUA)" mode, which attempts to avoid caching etc and just persist
+   the write. It may also be possible to provide additional
+   information about the "priority" of a write, or even a "time out"
+   associated with a write (if it hasn't happened by this time, then
+   don't perform the write at all).
+
+Effectively, a queue operates like a traditional block device.
+
+Traditional block devices are likely to be very slow; queue-based
+   interfaces such as those used in NVMe, are likely to be a much
+   better fit for ImpFS. Suppose we have a persistent object on which
+   we call the "sync" method. Then a traditional blk layer may already
+   have some cached writes related to that object. If the blk layer
+   does not know the connection between these writes and the object,
+   then a sync on the object must force a complete sync of the blk
+   layer (including all writes to all objects) which is potentially
+   very costly. If we assume a queue-based interface, then things are
+   much better, because the sync can target a particular queue (and
+   all writes for the object will be placed in that queue).
+
+In ImpFS, each object is provided with an interface that appears like
+   a traditional block device (with sync and possibly barrier
+   operations). However, the real block layer is assumed to be queue
+   based. Our current implementations of this on top of traditional
+   block devices are very inefficient because we effectively have only
+   one queue. However, we expect that porting the code to use a more
+   advanced queue-based interface would give a much greater
+   performance.
+
+It is perhaps worth putting some effort into emulating the queue-based
+   interface efficiently on top of a traditional block device. I'm not
+   sure how this should best be achieved, or whether it would actually
+   improve performance.
+
+*)
+
+
 (** {2 Kv ops} *)
 
 module Kvop = Kvop
